3f139cdfa73e4c0655f25cbc3e4e99cf8ac6e203
Solenrae <solenraxe@gmail.com>
2026-05-10 16:40:31 +0200

    Added version 1

---
+++ main.js
@@ -0,0 +1,104 @@
+let sentenceInput = document.getElementById("phrase-input");
+let bestSentenceP = document.getElementById("best-sentence");
+let sentencesP = document.getElementById("other-sentences");
+let genIndicator = document.getElementById("gen-indicator");
+
+let genNum = 0;
+let sentences = [];
+let bestSentence = "";
+
+let sentenceToMatch = "To be or not to be...";
+
+let sampleSize = 10000;
+
+function getRandomLetter() {
+    let letterNum = 32 + Math.floor(Math.random() * 94);
+    let letter = String.fromCharCode(letterNum);
+    return letter;
+}
+
+function getBest() {
+    let best = {
+        score: -100,
+        index: 0,
+    }
+    let best2 = {
+        score: -100,
+        index: 1,
+    }
+    for (let i=0; i<sentences.length; i++) {
+        let score = 0;
+        let sentence = sentences[i];
+        let lengthDiff = Math.abs(sentence.length - sentenceToMatch.length);
+        score -= lengthDiff;
+        let iterationLength = Math.min(sentence.length, sentenceToMatch.length);
+        for (let j=0; j<iterationLength; j++) {
+            if (sentence[j] === sentenceToMatch[j]) {
+                score += 1;
+            }
+        }
+        if (score > best.score) {
+            let previousBestScore = best.score;
+            let previousBestIndex = best.index;
+
+            best.score = score;
+            best.index = i;
+
+            best2.score = previousBestScore;
+            best2.index = previousBestIndex;
+        } else if (score > best2.score) {
+            best2.score = score;
+            best2.index = i;
+        }
+    }
+    return [sentences[best.index], sentences[best2.index]]
+}
+
+function onSentenceEntered() {
+    if (sentenceInput.value !== "") {
+        sentenceToMatch = sentenceInput.value;
+        genNum = 0;
+        genIndicator.textContent = "Generation " + genNum;
+    }
+}
+
+function generate() {
+    if (bestSentence === sentenceToMatch) {return;}
+    sentences = [];
+    if (genNum > 0) {
+        for (let i=0; i<sampleSize; i++) {
+            let newSentence = bestSentence;
+
+            let changeLength = Math.floor(Math.random() * 100) < 25;
+            let changeLetter = Math.floor(Math.random() * 100) < 75;
+            if (changeLetter) {
+                let letterToChange = Math.floor(Math.random() * newSentence.length);
+                let sentenceArray = newSentence.split("");
+                sentenceArray[letterToChange] = getRandomLetter();
+                newSentence = sentenceArray.join("");
+            }
+            if (changeLength) {
+                let direction = Math.floor(Math.random() * 2);
+                if (direction === 0) {newSentence = newSentence.substring(0, newSentence.length - 1);}
+                else if (direction === 1) {newSentence = newSentence + getRandomLetter()};
+            }
+            sentences.push(newSentence);
+        }
+    }
+    else {
+        for (let i=0; i<sampleSize; i++) {
+            let sentence = "";
+            let sentenceLength = 1 + Math.floor(Math.random() * 40);
+            for (let j=0; j<sentenceLength; j++) {
+                sentence = sentence + getRandomLetter();
+            }
+            sentences.push(sentence);
+        }
+    }
+    genNum++;
+    genIndicator.textContent = "Generation " + genNum;
+    let top = getBest();
+    bestSentence = top[0].slice(0, Math.round(top[0].length/2)) + top[1].slice(Math.round(top[1].length/2, top[1].length));
+    bestSentenceP.textContent = bestSentence;
+    //sentencesP.textContent = sentences.join(" ; ")
+}
